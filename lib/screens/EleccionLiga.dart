a5a4fca13079882ee6d2ee65aa49f359c9c04f71
velillaAV <0305avelilla@e-itaca.es>
2026-03-09 14:50:57 +0100

    fondo de pantalla, eleccion de liga y liga normal

--- lib/screens/EleccionLiga.dart
+++ lib/screens/EleccionLiga.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_intermodular/config/constantes/dimensions.dart';
+import 'package:proyecto_intermodular/screens/LigaNormal.dart';
 import 'package:proyecto_intermodular/widgets/Appbar.dart';
 import 'package:proyecto_intermodular/widgets/drawer.dart';
+
 
 class Eleccionliga extends StatefulWidget {
   const Eleccionliga({super.key});
@@ -11,26 +13,61 @@ class Eleccionliga extends StatefulWidget {
 }
 
 class _EleccionligaState extends State<Eleccionliga> {
+  void _ligaNormal(){
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const Liganormal(),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MyDrawer(),
-      appBar: PreferredSize(preferredSize: Size.fromHeight(Dimensiones.paddingAppbar), child: Appbar()),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(Dimensiones.paddingAppbar),
+        child: Appbar(),
+      ),
       body: Container(
-         decoration: BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("images/FondoMundial2026.png"),
             opacity: 0.2,
-            fit: BoxFit.fill,
+            fit: BoxFit.cover,
           ),
         ),
         child: Center(
-        
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              
-              SizedBox(height: 30,),
-              Text("ELIGE TIPO", style: TextStyle(fontSize: 80),)
+              SizedBox(height: 30),
+              Text("ELIGE TIPO", style: TextStyle(fontSize: 80)),
+              Container(
+                height: 150,
+                width: 350,
+                color: Colors.grey,
+                child: TextButton(
+                  onPressed: _ligaNormal,
+                  child: Text(
+                    "LIGA NORMAL",
+                    style: TextStyle(color: Colors.black, fontSize: 40),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                height: 150,
+                width: 350,
+                color: Colors.grey,
+                child: TextButton(
+                  onPressed: null,
+                  child: Text(
+                    "LIGA ESPECIAL",
+                    style: TextStyle(color: Colors.black, fontSize: 40),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
--- lib/screens/PantallaPrincipal.dart
+++ lib/screens/PantallaPrincipal.dart
@@ -13,102 +13,122 @@ class PantallaPrincipal extends StatefulWidget {
   State<PantallaPrincipal> createState() => _PantallaPrincipalState();
 }
 
-
-
 class _PantallaPrincipalState extends State<PantallaPrincipal> {
-
-  void _elegirLiga(){
+  void _elegirLiga() {
     Navigator.push(
-      context, 
-      MaterialPageRoute(builder: (context) => const Eleccionliga())
+      context,
+      MaterialPageRoute(builder: (context) => const Eleccionliga()),
     );
   }
 
-  
-
-void _informacionMundialista() {
-  Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => const Pantallainformacionmundialista()),
-  );
-}
-
+  void _informacionMundialista() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const Pantallainformacionmundialista(),
+      ),
+    );
+  }
 
   late String nombreUser = Logicausuario.getUsuarioActual().getNombre();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: MyDrawer(),
-      appBar: PreferredSize(preferredSize: Size.fromHeight(Dimensiones.paddingAppbar), child: Appbar()),
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/FondoMundial2026.png"),
-            opacity: 0.2,
-            fit: BoxFit.fill,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(Dimensiones.paddingAppbar),
+        child: Appbar(),
+      ),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "images/FondoMundial2026.jpg",
+              fit: BoxFit.cover,
+              opacity: const AlwaysStoppedAnimation(0.2),
+            ),
           ),
-        ),
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 30),
-              Column(
-                children: [
-                  Text(
-                    'Bienvenido a la Road to the FIFA World Cup 26 App, $nombreUser',
-                    style: TextStyle(fontSize: 40),
-                  ),
-                  SizedBox(height: 100),
-                  Text("LIGAS FANTASY" ,style: TextStyle(color: Colors.black, fontSize: 30)),
-                  SizedBox(height: 20,),
-                  Container(
-                    height: 100,
-                    width: 200,
-                    color: Colors.grey,
-                    child: TextButton(
-                      onPressed: _elegirLiga,
-                      child: Text("CREAR LIGA" ,style: TextStyle(color: Colors.black)),
-                      
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  Container(
-                    height: 70,
-                    width: 200,
-                    color: Colors.grey,
-                    child: TextButton(
-                      onPressed: _elegirLiga,
-                      child: Text("UNIRSE A LIGA", style: TextStyle(color: Colors.black)),
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  Container(
-                    height: 70,
-                    width: 200,
-                    color: Colors.grey,
-                    child: TextButton(
-                      onPressed: _elegirLiga,
-                      child: Text("TUS LIGAS" ,style: TextStyle(color: Colors.black)),
-                    ),
-                  ),
-                  SizedBox(height: 30),
-                  Text("INFORMACIÓN" ,style: TextStyle(color: Colors.black, fontSize: 30)),
-                  SizedBox(height: 10),
-                  Container(
-                    height: 70,
-                    width: 250,
-                    color: Colors.grey,
-                    child: TextButton(
-
-                      onPressed: _informacionMundialista,
-                      child: Text("INFORMACIÓN MUNDIALISTA" ,style: TextStyle(color: Colors.black)),
-                    ),
-                  )
-                ],
+          SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height,
               ),
-            ],
+              child: Center(
+                child: Column(
+                  children: [
+                    SizedBox(height: 30),
+                    Text(
+                      'Bienvenido a la Road to the FIFA World Cup 26 App, $nombreUser',
+                      style: TextStyle(fontSize: 40),
+                    ),
+                    SizedBox(height: 40),
+                    Text(
+                      "LIGAS FANTASY",
+                      style: TextStyle(color: Colors.black, fontSize: 30),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 70,
+                      width: 200,
+                      color: Colors.grey,
+                      child: TextButton(
+                        onPressed: _elegirLiga,
+                        child: Text(
+                          "CREAR LIGA",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      height: 70,
+                      width: 200,
+                      color: Colors.grey,
+                      child: TextButton(
+                        onPressed: _elegirLiga,
+                        child: Text(
+                          "UNIRSE A LIGA",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Container(
+                      height: 70,
+                      width: 200,
+                      color: Colors.grey,
+                      child: TextButton(
+                        onPressed: _elegirLiga,
+                        child: Text(
+                          "TUS LIGAS",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      "INFORMACIÓN",
+                      style: TextStyle(color: Colors.black, fontSize: 30),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      height: 70,
+                      width: 250,
+                      color: Colors.grey,
+                      child: TextButton(
+                        onPressed: _informacionMundialista,
+                        child: Text(
+                          "INFORMACIÓN MUNDIALISTA",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
